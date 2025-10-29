ba0bf0f2997b340bb93f5e58e1a73e924aa44f4d
Robin Kohrs <robin.kohrs@gmx.de>
2025-10-29 16:25:17 +0100

    ..

--- create_swipers.R
+++ create_swipers.R
@@ -4,25 +4,33 @@ library(glue)
 library(sf)
 library(davR)
 library(jsonlite)
+library(fs)
 
 
-# Define the list of images for the swiper.
-# The URLs are based on the link you provided and your mention of "output 1-4".
-image_list <- list(
-  list(path = "https://raw.githubusercontent.com/RobinKohrs/glyph/refs/heads/main/207920/output_1.png"),
-  list(path = "https://raw.githubusercontent.com/RobinKohrs/glyph/refs/heads/main/207920/output_2.png"),
-  list(path = "https://raw.githubusercontent.com/RobinKohrs/glyph/refs/heads/main/207920/output_3.png"),
-  list(path = "https://raw.githubusercontent.com/RobinKohrs/glyph/refs/heads/main/207920/output_4.png")
-)
+# Define a vector of plot IDs to process
+plot_ids <- c("207920", "270924") # Add other IDs here as you get them
 
-# Generate the image swiper widget.
-# This calls the function with your image data. No overlay is specified.
-swiper_widget <- html_create_image_swiper(
-  image_data = image_list, gradient_color = "#fff",
-  aspect_ratio = "16/9"
-)
+# Loop through each plot ID
+for (plot_id in plot_ids) {
+  # Define the list of images for the swiper using relative paths
+  image_paths <- fs::dir_ls(here::here("docs", "data", plot_id), glob = "*.png")
 
-# To display the widget, simply call the variable in your console,
-# or include it as the last line in an R Markdown chunk.
-swiper_widget %>% clipr::write_clip()
-writeLines(swiper_widget, sys_make_path("docs/index.html"))
+  image_list <- lapply(image_paths, function(path) {
+    list(path = paste0("../data/", plot_id, "/", basename(path)))
+  })
+
+  # Generate the image swiper widget.
+  # This calls the function with your image data. No overlay is specified.
+  swiper_widget <- html_create_image_swiper(
+    image_data = image_list,
+    gradient_color = "#fff",
+    aspect_ratio = "1/1",
+    max_width = "822"
+  )
+
+  # Define the output path for the swiper HTML file
+  output_file <- here::here("docs", "swipers", paste0(plot_id, ".html"))
+
+  # Write the widget to the file
+  writeLines(as.character(swiper_widget), output_file)
+}
